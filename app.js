9200efb6fbeac1d4cda25ccef86d377acabe598a
Bruno Correia <brunocorreia@gmail.com>
2019-12-11 11:29:30 +0100

    formular erstellt

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 let app = express();
 
 app.set("view engine", "ejs");
-app.set("views", __dirname + "views");
+app.set("views", __dirname + "/views");
 
 app.get('/', function(req, res){
     res.send("Hello");
 });
+
+app.get('/eingabe', function(req, res){
+    res.render('eingabe');
+})
 
 app.listen(8080);
