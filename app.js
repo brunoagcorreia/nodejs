07aa77f3d67c854176e147d282a3e82963f4817e
Bruno Correia <brunocorreia@gmail.com>
2019-12-12 10:12:37 +0100

    app_old = faker test

--- app.js
+++ app.js
@@ -1,29 +1,63 @@
+// import Modul: Webserver (Express), POST encoder (body-parser)
 const express = require("express");
-let app = express();
-let bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+let models = require("./models");
 
+// Webserver inistialisieren
+let app = express();
+
+
+
+// benutze die Funktion JSON & URLencoder von body-parser, 
+// um POST Daten auszulesen und in eine JSON Objekt umzuwandeln
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+models.Person.sync({force: true}).then(function() {
+    console.log("Tabelle wurde erstellt");
+    
+
+});
+// Benutze EJS Interpreter um JS in 
+// HTML File Serverseitig zu verarbeiten
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-app.use(bodyParser.json()); // to support Json encoded bodies
-app.use(bodyParser.urlencoded({ 
-    extended: true
-})); //
-
+// Routing: definiert das Weiterleiten bzw. Aktion
+// beim Aufruf einer bestimmen URL 
+// .get - bei Aufruf der GET Methode
+// .post - bei Aufruf der POST Methode
 app.get('/', function(req, res){
-    res.send("Hello");
+    res.send("Hallo");
 });
 
 app.get('/eingabe', function(req, res){
-    res.render('eingabe');
+    res.render("eingabe");
 });
 
 app.post('/db_input', function(req, res){
     console.log(req.body);
-    // let erg = req.body.vorname
-    // let erg2 = req.body.nachname
-
-    res.render("db_input", {vorname: req.body.vorname})
+    console.log(models.sequelize);
+    models.Person.create({			// Eintrag in Tabelle einfügen
+        vorname: req.body.vorname,
+        nachname: req.body.nachname,
+        email: req.body.email,
+        })
+   res.redirect("/db_output") ;
 });
+
+app.get('/db_output', function(req, res){
+    models.Person.findAll({
+        where: {
+            id: 1
+        }
+    }).then(function(obj){
+        console.log(obj[0].dataValues.vorname)
+        res.render("ausgabe", {person: obj[0].dataValues});
+    });
+        
+})
 
 app.listen(8080);
---
+++ app_old.js
@@ -0,0 +1,50 @@
+const express = require("express");
+let bodyParser = require("body-parser")
+let models = require("./models");
+let faker = require("faker")
+
+let app = express();
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(bodyParser.json()); // to support Json encoded bodies
+app.use(bodyParser.urlencoded({ 
+    extended: true
+})); //
+
+models.Person.sync({force: true}).then(function() {
+    console.log("Tabelle wurde erstellt");
+});
+
+app.get('/', function(req, res){
+    res.send("Hello");
+});
+
+app.get('/eingabe', function(req, res){
+    res.render('eingabe');
+});
+
+app.post('/db_input', function(req, res){
+    console.log(req.body);
+    // let erg = req.body.vorname
+    // let erg2 = req.body.nachname
+
+    // models.Person.create({			// Eintrag in Tabelle einfügen
+    //     vorname: req.body.vorname,
+    //     nachname: req.body.nachname,
+    //     email: req.body.email,
+    //     })
+
+    for (let i = 1; i <= 20; ++i) {
+        models.Person.create({			// Eintrag in Tabelle einfügen
+            vorname: req.body.vorname,
+            nachname: req.body.nachname,
+            email: req.body.email,
+            iban: req.body.email,
+        });
+    }
+    res.render("db_input", {vorname: req.body.vorname, nachname: req.body.nachname, email: req.body.email})
+});
+
+app.listen(8080);
